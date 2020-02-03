387a2c19f9a56e751581a0679d961227d313481a
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-02-03 01:13:24 -0500

    created a new view and updated home.html:

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+//require auto-load file
+require ("vendor/autoload.php");
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Create an instance of the base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+    //echo "<h1>Hello Dating</h1>";
+});
