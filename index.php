d0953ca9e1041283d64298ef83f1d7e72c1922fe
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-02-03 01:25:00 -0500

    Added some bootstrap styling

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+
 //require auto-load file
 require ("vendor/autoload.php");
+
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -14,3 +16,7 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
     //echo "<h1>Hello Dating</h1>";
 });
+
+//Run F3
+$f3->run();
+
