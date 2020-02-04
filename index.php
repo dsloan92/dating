a72ab38ca7a36aea70497600f07b911937540157
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-02-04 03:16:12 -0500

    Added a results page as well as an interests page. Index now saves all variables in session variable

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 //require auto-load file
 require ("vendor/autoload.php");
+session_start();
 
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
@@ -34,17 +35,40 @@ $f3->route('POST /info', function(){
 
 //Define a Profile Route
 $f3->route('POST /profile', function(){
+    $_SESSION['first']=$_POST['first'];
+    $_SESSION['last']=$_POST['last'];
+    $_SESSION['age']=$_POST['age'];
+    $_SESSION['gender']=$_POST['gender'];
+    $_SESSION['number']=$_POST['number'];
+
     $view = new Template();
+
     echo $view->render('views/profile.html');
     //echo "<h1>Hello Dating</h1>";
 });
 
 //Define a Interests Route
 $f3->route('POST /interests', function(){
+    $_SESSION['email']=$_POST['email'];
+    $_SESSION['state']=$_POST['state'];
+    $_SESSION['seeking']=$_POST['seeking'];
+    $_SESSION['biography']=$_POST['biography'];
+
     $view = new Template();
     echo $view->render('views/interests.html');
     //echo "<h1>Hello Dating</h1>";
 });
+
+//Define a Summary Route
+$f3->route('POST /summary', function(){
+    $_SESSION['inDoor']=$_POST['inDoor'];
+    $_SESSION['outDoor']=$_POST['outDoor'];
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+    //echo "<h1>Hello Dating</h1>";
+});
+
 
 
 
