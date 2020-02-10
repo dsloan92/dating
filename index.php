cc6da342515b7c89c3d048c3f894b1c410ca0153
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-02-10 02:12:20 -0500

    created a validation page validating the requested information

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
     Filename:           index.php
 */
 
-
-//require auto-load file
-require ("vendor/autoload.php");
+//start a session
 session_start();
+//require auto-load file
+
+require_once ("vendor/autoload.php");
+require_once("model/validation.php");
 
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
@@ -27,19 +29,68 @@ $f3->route('GET /', function(){
 });
 
 //Define a Personal Information Route
-$f3->route('POST /info', function(){
+$f3->route('GET|POST /info', function($f3){
+    //Checking to see if form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        //var_dump($_POST);
+
+        //grabbing data from form
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $age = $_POST['age'];
+        $phone = $_POST['number'];
+
+        //adding data to hive
+        $f3->set('first', $first);
+        $f3->set('last', $last);
+        $f3->set('age', $age);
+        $f3->set('phone', $phone);
+
+        //check if form is valid
+        if (validInfoForm()){
+            //write data to session
+            $_SESSION['first']=$first;
+            $_SESSION['last']=$last;
+            $_SESSION['age']=$age;
+            $_SESSION['phone']=$phone;
+
+            //redirect to profile page
+            $f3->reroute('/profile');
+        }
+    }
+
+
     $view = new Template();
     echo $view->render('views/personalInfo.html');
     //echo "<h1>Hello Dating</h1>";
 });
 
 //Define a Profile Route
-$f3->route('POST /profile', function(){
-    $_SESSION['first']=$_POST['first'];
-    $_SESSION['last']=$_POST['last'];
-    $_SESSION['age']=$_POST['age'];
-    $_SESSION['gender']=$_POST['gender'];
-    $_SESSION['number']=$_POST['number'];
+$f3->route('POST|GET /profile', function($f3){
+    echo $_SERVER['REQUEST_METHOD'];
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        //grabbing data from form
+        $email = $_POST['email'];
+
+        //adding to hive
+        $f3->set('email', $email);
+
+        //checking to ensure form is valid
+        if (validateProfileForm()){
+            //write data to session
+            $_SESSION['email'] = $email;
+
+            //redirect to interests page
+            $f3->reroute('/interests');
+        }
+    }
+
+
+        /*$_SESSION['first']=$_POST['first'];
+        $_SESSION['last']=$_POST['last'];
+        $_SESSION['age']=$_POST['age'];
+        $_SESSION['gender']=$_POST['gender'];
+        $_SESSION['number']=$_POST['number'];*/
 
     $view = new Template();
 
@@ -48,7 +99,7 @@ $f3->route('POST /profile', function(){
 });
 
 //Define a Interests Route
-$f3->route('POST /interests', function(){
+$f3->route('POST|GET /interests', function(){
     $_SESSION['email']=$_POST['email'];
     $_SESSION['state']=$_POST['state'];
     $_SESSION['seeking']=$_POST['seeking'];
--- model/validation.php
+++ model/validation.php
@@ -1,1 +1,120 @@
 <?php
+
+//creating multiple functions to validate different pieces of form data
+
+
+//validating the Info Form
+function validInfoForm()
+{
+    //creating a isValid boolean
+    $isValid = true;
+    global $f3;
+
+    if (!validName($f3->get('last'))) {
+        $isValid = false;
+        $f3->set("errors['last']", "Please enter a valid Last Name");
+    }
+
+    if (!validName($f3->get('first'))) {
+        $isValid = false;
+        $f3->set("errors['first']", "Please enter a valid First Name");
+    }
+
+    if (!validAge($f3->get('age'))) {
+        $isValid = false;
+        $f3->set("errors['age']", "Please enter a valid Age");
+    }
+
+    if (!validPhone($f3->get('phone'))) {
+        $isValid = false;
+        $f3->set("errors['phone']", "Please enter a valid Phone Number");
+    }
+    return $isValid;
+
+}
+
+//Validating the Profile form
+function validateProfileForm()
+{
+    $isValid = true;
+    global $f3;
+    if (!validEmail($f3->get('email'))) {
+        $isValid = false;
+        $f3->set("errors['email']", "Please enter a valid Email");
+    }
+
+    return $isValid;
+}
+
+//validating Interest Form
+function validateInterestForm(){
+    $isValid = true;
+    global $f3;
+    if (!validOutDoor($f3->get('selectedOutDoorOptions'))){
+        $isValid = false;
+        $f3->set("errors['selectedOutDoorOptions']", "Please choose valid OutDoor Options");
+    }
+
+    if (!validInDoor($f3->get('selectedInDoorOptions'))){
+        $isValid = false;
+        $f3->set("errors['selectedIntDoorOptions']", "Please choose valid InDoor Options");
+    }
+
+    return $isValid;
+}
+
+//validating that a string is alphabetic
+function validName($name)
+{
+
+    return !empty($name) && ctype_alpha($name);
+}
+
+//validating the age input is between 18 and 118
+function validAge($age)
+{
+    return !empty($age) && ctype_digit($age) && $age >= 18 & $age <= 118;
+}
+
+//validating phone numbers
+function validPhone($phone)
+{
+    //remove basic phone characters
+    $phone = str_replace(" ", "", $phone);
+    $phone = preg_replace('/-/', "", $phone);
+    $phone = preg_replace('/\(/', "", $phone);
+    $phone = preg_replace('/\)/', "", $phone);
+    $phone = preg_replace('/_/', "", $phone);
+    //echo "<p>$id</p>"; for de-bugging purposes
+
+    return !empty($phone) && ctype_digit($phone) && strlen($phone) === 10;
+
+}
+
+//validate email
+function validEmail($email)
+{
+    return (!empty($email) && preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', $email));
+
+}
+
+//validate out door checkbox
+function validOutDoor($selectedOutDoorOptions){
+    global $outDoorOptions;
+    foreach ($selectedOutDoorOptions as $value){
+        if(!in_array($value, $outDoorOptions)){
+            return false;
+        }
+    }
+}
+
+//validate inDoor checkbox
+function validInDoor($selectedInDoorOptions){
+    global $inDoorOptions;
+    foreach ($selectedInDoorOptions as $value){
+        if(!in_array($value, $inDoorOptions)){
+            return false;
+        }
+    }
+}
+
